03a35e21fc7a90114827c1cd3807bbcbac278ce2
Anastasia Saiz <71260261+AnastasiaSaiz@users.noreply.github.com>
2020-12-22 12:33:20 +0100

    modif

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
+const Cartas = (props) => {
+  const [cards, setCards] = useState([])
+  useEffect(() => {
+    fetch("https://api.magicthegathering.io/v1/cards?set=" + props.selected)
+      .then((res) => res.json())
+      .then((res) => {
+        setCards(res.cards);
+        console.log(res)
+      });
+  }, [props.selected])
+
+  const mostrarCartas=cards.map(card=>{
+    return(
+      <div>
+        <h4>{card.name}</h4>
+        <p>{card.text}</p>
+        <img class="carta" src={card.imageUrl} alt="" />
+      </div>
+    )
+  })
+  return mostrarCartas;
+}
 
 function App() {
+  const [data, setData] = useState([]);
+  const [selected, setSelected] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.magicthegathering.io/v1/sets")
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.sets)
+      });
+  }, []);
+
+  const manageChange = (e) => {
+    setSelected(e.target.value)
+  }
+
+  const options = data.map(option => {
+    return <option value={option.code}>{option.name}</option>
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <select onChange={manageChange}>{options}</select>
+      <Cartas selectes={selected} />
+    </>
+  )
 }
 
 export default App;
